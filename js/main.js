9b78947906817ab906ec17dae789bd3c4ee5dfec
huydhuynh <47584327+huynhduchuydp36@users.noreply.github.com>
2022-09-29 08:21:44 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -120,7 +120,7 @@ $('#yes').click(function() {
         background: '#fff url("img/iput-bg.jpg")',
         backdrop: `
               rgba(0,0,123,0.4)
-              url("img/giphy3.gif")
+              url("img/giphy4.gif")
               left top
               no-repeat
             `,
